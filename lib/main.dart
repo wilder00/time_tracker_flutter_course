70037ded9d4e17b8915641ae728645823a67c0ed
Wilder Trujillo <nadie437@gmail.com>
2021-03-01 14:42:25 -0500

    Creando un landing page como mediador para el sign in y el home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_course/app/landing_page.dart';
 
 //si usamos async await debemos devolver un Future
 Future<void> main() async {
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: SignInPage(),
+      home: LandingPage(),
     );
   }
 }
