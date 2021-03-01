b7f830f16001126ae1527524d4576e6205ce7e5f
Wilder Trujillo <nadie437@gmail.com>
2021-03-01 15:59:19 -0500

    creando la clase Auth y  una clase abstracta AuthBase, todo listo para la autenticación de forma inyectada La anterior forma de autenticar era de forma global

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Auth {
+abstract class AuthBase {
+  User get currentUser;
+  Future<User> signInAnonimously();
+  Future<void> signOut();
+}
+
+class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
 
+  @override
   User get currentUser => _firebaseAuth.currentUser;
 
+  @override
   Future<User> signInAnonimously() async {
     final userCredential = await _firebaseAuth.signInAnonymously();
     return userCredential.user;
   }
 
+  @override
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
