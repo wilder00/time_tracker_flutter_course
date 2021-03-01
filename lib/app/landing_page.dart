e8413454875311b903fff6626d44901e99e2e8b5
Wilder Trujillo <nadie437@gmail.com>
2021-03-01 15:05:55 -0500

    El boton de autenticación anonymously funcionagit statusgit statusgit status!

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -10,10 +10,18 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   User _user;
 
+  void _updateUser(User user) {
+    setState(() {
+      _user = user;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     if (_user == null) {
-      return SignInPage();
+      return SignInPage(
+        onSignIn: _updateUser,
+      );
     }
     return Container(); // temporary placeholder for HomePage
   }
